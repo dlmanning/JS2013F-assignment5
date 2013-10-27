a283e180196ebf10b9553c9aa08a24b3cb80b89e
JFingert <joshuafingert@gmail.com>
2013-10-27 13:26:26 -0700

    initial commit

---
+++ fingert/roadSign.js
@@ -0,0 +1,22 @@
+function roadSign (text, color, shape) {
+	this.text = text;
+	this.color = color;
+	this.shape = shape;
+}
+
+roadSign.prototype.manufacture = function () {
+  if (this.text == "Speed Limit"){
+    var sen = "It's a " + this.text + " sign, which is " + this.color + " and Nerdy.";
+  } else{
+	var sen = "It's a " + this.text + " sign, which is " + this.color + " and " + this.shape;
+    }
+  return sen;
+}
+
+var yield = new roadSign ("Yield", "Yellow", "Triangular");
+var stop = new roadSign ("Stop", "Red", "Octogonal");
+var speedLimit = new roadSign ("Speed Limit", "White", "Square");
+console.log(yield.manufacture());
+console.log(stop.manufacture());
+console.log(speedLimit.manufacture());
+
