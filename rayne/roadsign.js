d55589deb3ac3cfc2399662949c5535d75f7b721
kaijarayne <kaijarayne@gmail.com>
2013-10-27 22:16:52 -0700

    adding roadsign assignment

---
+++ rayne/roadsign.js
@@ -0,0 +1,17 @@
+function RoadSign (text, color, shape) {
+	this.text = text;
+	this.color = color;
+	this.shape = shape;
+}
+
+RoadSign.prototype.whatSign = function () {
+	console.log("This sign says " + this.text + ". It is a " + this.color + " " + this.shape + ".");
+}
+
+var yieldSign = new RoadSign ("YIELD", "Red and White", "Triangle");
+var stopSign = new RoadSign ("STOP", "Red and White", "Octagon");
+var speedLimitSign = new RoadSign ("SPEED LIMIT", "White and Black", "Rectangle");
+
+console.log(yieldSign.whatSign());
+console.log(stopSign.whatSign());
+console.log(speedLimitSign.whatSign());
