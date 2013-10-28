94ae23566a793484cce6f39b531ea5a440d78e06
curriedav <curriedav@gmail.com>
2013-10-28 14:19:56 -0700

    updates recordMaker.js

--- currie/recordMaker.js
+++ currie/recordMaker.js
@@ -1,4 +1,4 @@
-function record (name, address, phoneNumber, age, wombat) {
+function Record (name, address, phoneNumber, age, wombat) {
 	this.name = name;
 	this.address = address;
 	this.phoneNumber = phoneNumber;
@@ -6,19 +6,19 @@ function record (name, address, phoneNumber, age, wombat) {
 	this.wombat = wombat;
 };
 
-var person01 = new record ("David", "Portland, OR", "(703) 624-8919", 25, true);
-var person02 = new record ("Frankie", "Sterling, VA", "(555) 555-5555", 26, true);
-var person03 = new record ("Steve", "Arlington, VA", "(555) 555-5555", 27, false);
+var person01 = new Record ("David", "Portland, OR", "(703) 624-8919", 25, true);
+var person02 = new Record ("Frankie", "Sterling, VA", "(555) 555-5555", 26, true);
+var person03 = new Record ("Steve", "Arlington, VA", "(555) 555-5555", 27, false);
 
 
-function recordMaker (record) {
-  console.log(record.name + " is " + record.age + " years old, and lives in " 
-  	+ record.address);
-  if (record.wombat) {
-    console.log("Calling " + record.name + " at " + record.phoneNumber 
+function recordMaker (recordObj) {
+  console.log(recordObj.name + " is " + recordObj.age + " years old, and lives in " 
+  	+ recordObj.address);
+  if (recordObj.wombat) {
+    console.log("Calling " + recordObj.name + " at " + recordObj.phoneNumber 
     	+ " about their wombat");
   } else {
-    console.log(record.name + " doesn't have a wombat");
+    console.log(recordObj.name + " doesn't have a wombat");
   }
 }
 
